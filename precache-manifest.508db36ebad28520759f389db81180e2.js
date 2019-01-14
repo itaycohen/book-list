70042c04d9bc2a13ca9a61034fb8fad61c6331e8
Itay Cohen <itay2002@gmail.com>
2019-01-14 12:46:25 +0700

    Updates

--- precache-manifest.508db36ebad28520759f389db81180e2.js
+++ precache-manifest.508db36ebad28520759f389db81180e2.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/the-books-of-medium/static/media/outline-icons.701ae6ab.eot"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/the-books-of-medium/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "10d51a3ddd3d751d8ed3",
@@ -24,24 +24,24 @@ self.__precacheManifest = [
     "url": "/the-books-of-medium/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/the-books-of-medium/static/media/icons.0ab54153.woff2"
-  },
-  {
     "revision": "9c74e172f87984c48ddf5c8108cabe67",
     "url": "/the-books-of-medium/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/the-books-of-medium/static/media/icons.0ab54153.woff2"
   },
   {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/the-books-of-medium/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/the-books-of-medium/static/media/icons.b87b9ba5.ttf"
-  },
-  {
     "revision": "faff92145777a3cbaf8e7367b4807987",
     "url": "/the-books-of-medium/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/the-books-of-medium/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "10d51a3ddd3d751d8ed3",
@@ -52,24 +52,16 @@ self.__precacheManifest = [
     "url": "/the-books-of-medium/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/the-books-of-medium/static/media/outline-icons.ad97afd3.ttf"
-  },
-  {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/the-books-of-medium/static/media/icons.962a1bf3.svg"
-  },
-  {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/the-books-of-medium/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/the-books-of-medium/static/media/outline-icons.82f60bd0.svg"
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/the-books-of-medium/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/the-books-of-medium/static/media/brand-icons.13db00b7.eot"
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/the-books-of-medium/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
@@ -78,6 +70,14 @@ self.__precacheManifest = [
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
     "url": "/the-books-of-medium/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/the-books-of-medium/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/the-books-of-medium/static/media/icons.962a1bf3.svg"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",

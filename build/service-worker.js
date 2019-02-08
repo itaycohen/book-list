d4898b31b8d21404534b5a25e8ec37ce247d03a1
Itay Cohen <itay2002@gmail.com>
2019-02-08 16:41:29 +0700

    push

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f42cdf323d0fb421e62033ceb6ee5809.js"
+  "/precache-manifest.bb632bbacc63096d004fe1a69e232ef3.js"
 );
 
 workbox.clientsClaim();
